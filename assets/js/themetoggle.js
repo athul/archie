d0cada69a6bd1abba0cfecae932a3cf17cac213d
Ananth Bhaskararaman <antsub@gmail.com>
2024-07-27 15:00:00 +0530

    fix: feather icons relative url issue
    Loading feather icons from JS messed up relative URLs.
    We now load all icons in HTML as hidden and toggle visibility in JS.

--- assets/js/themetoggle.js
+++ assets/js/themetoggle.js
@@ -4,44 +4,71 @@ const itemTheme = "theme-storage";
 const themeLight = "light";
 const themeDark = "dark";
 
-const iconSunrise = `<svg class="feather"><use href="../feather-icons/feather-sprite.svg#sunrise"></use></svg>`;
-const iconSunset = `<svg class="feather"><use href="../feather-icons/feather-sprite.svg#sunset"></use></svg>`;
-const iconSun = `<svg class="feather"><use href="../feather-icons/feather-sprite.svg#sun"></use></svg>`;
-const iconMoon = `<svg class="feather"><use href="../feather-icons/feather-sprite.svg#moon"></use></svg>`;
-
-const idToggle = "colour-scheme-toggle";
-
 /**
  * Set the theme based on the mode.
  * If mode is null, the theme depends on the device colour scheme.
  */
 function setTheme(mode) {
-    const modeToggle = document.getElementById(idToggle);
     const modeStyle = document.getElementById("dark-mode-style");
 
     const sunriseOrSunset = e => {
         e.matches ?
-            modeToggle.innerHTML = iconSunset :
-            modeToggle.innerHTML = iconSunrise;
+            showIcon("sunset") :
+            showIcon("sunrise");
     }
 
     window.matchMedia(queryPrefersDark).removeEventListener("change", sunriseOrSunset);
 
     if (mode === null) {
-        modeToggle.innerHTML = window.matchMedia(queryPrefersDark).matches ?
-            iconSunset :
-            iconSunrise;
+        window.matchMedia(queryPrefersDark).matches ?
+            showIcon("sunset") :
+            showIcon("sunrise");
         window.matchMedia(queryPrefersDark).addEventListener("change", sunriseOrSunset);
         modeStyle.media = queryPrefersDark;
         modeStyle.disabled = false;
         localStorage.removeItem(itemTheme);
     } else {
         mode === themeDark ?
-            modeToggle.innerHTML = iconMoon :
-            modeToggle.innerHTML = iconSun;
+            showIcon("moon") :
+            showIcon("sun");
         modeStyle.disabled = mode === themeLight;
         modeStyle.removeAttribute("media");
         localStorage.setItem(itemTheme, mode);
+    }
+}
+
+function showIcon(icon) {
+    const iconSunrise = document.getElementById("colour-scheme-toggle-sunrise");
+    const iconSunset = document.getElementById("colour-scheme-toggle-sunset");
+    const iconSun = document.getElementById("colour-scheme-toggle-sun");
+    const iconMoon = document.getElementById("colour-scheme-toggle-moon");
+
+    const isHidden = "is-hidden";
+    switch (icon) {
+        case "sunrise":
+            iconSunrise.classList.remove(isHidden);
+            iconSunset.classList.add(isHidden);
+            iconSun.classList.add(isHidden);
+            iconMoon.classList.add(isHidden);
+            break;
+        case "sunset":
+            iconSunrise.classList.add(isHidden);
+            iconSunset.classList.remove(isHidden);
+            iconSun.classList.add(isHidden);
+            iconMoon.classList.add(isHidden);
+            break;
+        case "sun":
+            iconSunrise.classList.add(isHidden);
+            iconSunset.classList.add(isHidden);
+            iconSun.classList.remove(isHidden);
+            iconMoon.classList.add(isHidden);
+            break;
+        case "moon":
+            iconSunrise.classList.add(isHidden);
+            iconSunset.classList.add(isHidden);
+            iconSun.classList.add(isHidden);
+            iconMoon.classList.remove(isHidden);
+            break;
     }
 }
 
@@ -61,7 +88,7 @@ function toggleTheme() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const toggleBtn = document.getElementById(idToggle);
+    const toggleBtn = document.getElementById("colour-scheme-toggle");
     toggleBtn.addEventListener("click", toggleTheme);
 
     const savedTheme = localStorage.getItem(itemTheme);
