bc86375a70b1b7010e14c581e00b9025253d836d
Ananth Bhaskararaman <antsub@gmail.com>
2024-07-27 12:58:50 +0530

    feat(theme): Different icons for auto-light and auto-dark
    If scheme is detected as light, use sunrise icon. Otherwise
    use sunset icon.

--- assets/js/themetoggle.js
+++ assets/js/themetoggle.js
@@ -1,8 +1,11 @@
+const queryPrefersDark = "(prefers-color-scheme: dark)";
+
 const itemTheme = "theme-storage";
 const themeLight = "light";
 const themeDark = "dark";
 
-const iconAuto = `<i data-feather="refresh-cw"></i>`;
+const iconSunrise = `<i data-feather="sunrise"></i>`;
+const iconSunset = `<i data-feather="sunset"></i>`;
 const iconDark = `<i data-feather="moon"></i>`;
 const iconLight = `<i data-feather="sun"></i>`;
 
@@ -16,9 +19,21 @@ function setTheme(mode) {
     const modeToggle = document.getElementById(idToggle);
     const modeStyle = document.getElementById("dark-mode-style");
 
+    const sunriseOrSunset = e => {
+        e.matches ?
+            modeToggle.innerHTML = iconSunset :
+            modeToggle.innerHTML = iconSunrise;
+        feather.replace();
+    }
+
+    window.matchMedia(queryPrefersDark).removeEventListener("change", sunriseOrSunset);
+
     if (mode === null) {
-        modeToggle.innerHTML = iconAuto;
-        modeStyle.media = "(prefers-color-scheme: dark)";
+        modeToggle.innerHTML = window.matchMedia(queryPrefersDark).matches ?
+            iconSunset :
+            iconSunrise;
+        window.matchMedia(queryPrefersDark).addEventListener("change", sunriseOrSunset);
+        modeStyle.media = queryPrefersDark;
         modeStyle.disabled = false;
         localStorage.removeItem(itemTheme);
     } else {
@@ -30,7 +45,7 @@ function setTheme(mode) {
         localStorage.setItem(itemTheme, mode);
     }
 
-    feather.replace()
+    feather.replace();
 }
 
 /**
